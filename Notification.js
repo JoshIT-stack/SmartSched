cb50a70e955cff2b59fb9285c945830e107c54be
Gieben397276 <magtotogieben@gmail.com>
2025-11-13 05:37:35 +0800

    Create Notification.js

---
+++ Notification.js
@@ -0,0 +1,10 @@
+const modal = document.getElementById("notificationModal");
+const btn = document.getElementById("notificationBtn");
+
+btn.onclick = function () {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
